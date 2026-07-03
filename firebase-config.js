aa9b54b62a63e4a234bf5bb91fe2244166d6156e
connectbazarhelp-pixel <connectbazarhelp@gmail.com>
2026-07-04 03:34:30 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// ConnectBazar Firebase Config
+// Firebase Console > Project Settings > General > Your apps > Web app config
+window.CB_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBxXP8asrvUVPxWUBQ3NFhVW38UGeFTKZ8",
+  authDomain: "connectbazar-893fc.firebaseapp.com",
+  projectId: "connectbazar-893fc",
+  storageBucket: "connectbazar-893fc.firebasestorage.app",
+  messagingSenderId: "908554431469",
+  appId: "1:908554431469:web:6ad9cfc134431b2af255e4",
+  measurementId: "G-EBGY7XJVF9"
+};
